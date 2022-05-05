6a96db87bc2ad0365d1ba46b35e7365f2549dd9e
awidiyadew <awidiya.dewa@gmail.com>
2022-05-05 15:53:52 +0700

    (exam): solve exercise 3

--- src/exercise-3.js
+++ src/exercise-3.js
@@ -1,5 +1,9 @@
 const getNumberType = (number) => {
-  // implement code here
+  if (number <= 0) {
+    return null;
+  }
+  const isEvenNumber = number % 2 === 0;
+  return isEvenNumber ? 'even number' : 'odd number';
 };
 
 export default getNumberType;
